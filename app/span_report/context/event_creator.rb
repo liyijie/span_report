46588ea9e108cff598f80e3aa3b5ccd1de9e62d6
liyijie <liyijie825@gmail.com>
2013-10-22 11:07:15 +0800

    new event_creator.rb file

---
+++ app/span_report/context/event_creator.rb
@@ -0,0 +1,19 @@
+# encoding: utf-8
+
+module SpanReport::Context
+  class EventCreator < BaseContext
+
+    def load_relate
+      
+    end
+
+    # * create the rules
+    # * sort the rules, ie or singal
+    # * if has any signal rule, read signal datas
+    # * if has any ie rule, read small file datas
+    # * rewrite the event file
+    def process
+      
+    end
+  end
+end
