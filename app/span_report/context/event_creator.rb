a4d8c3cbcbe71e2887c841768b4add6f1fa5116e
liyijie <liyijie825@gmail.com>
2013-10-22 18:03:00 +0800

    ie rule logical complete

--- app/span_report/context/event_creator.rb
+++ app/span_report/context/event_creator.rb
@@ -18,25 +18,33 @@ module SpanReport::Context
       # * create the rules
       signal_rule = SignalRule.new signal_name: "RRCConnectionReconfiguration",
                                     event_name: "RRC Reconfig"
+      ie_rule = IEDurationRule.new iename: "i637", event_name: "RSRP low", duration: 3,
+                                  threshold: -100, compare_type: 1
       # * sort the rules, ie or singal
       # * read the orign events from event file
       event_file = File.join @input_log, "event"
-      signal_file = File.join @input_log, "signal"
       events = EventCsv.foreach(event_file)
+
+      # * if has any signal rule, read signal datas and process
+      signal_file = File.join @input_log, "signal"
       SignalCsv.foreach(signal_file) do |signal|
         if signal_rule.valid? signal
           event_added = signal_rule.create_event signal
           merge_event events, event_added
         end
       end
-
-      new_event_file = File.join @output_log, "event.new"
-      EventCsv.output(new_event_file, events)
-
-      # * if has any signal rule, read signal datas and process
       # * if has any ie rule, read map csv file datas and process
+      map_csv_file = File.join @input_log, "map.csv"
+      MapCsv.foreach(map_csv_file) do |data|
+        if ie_rule.valid? data
+          event_added = ie_rule.create_event data
+          merge_event events, event_added
+        end
+      end
       # * insert the created event into the orgin signals
+      new_event_file = File.join @output_log, "event.new"
       # * rewrite the event file
+      EventCsv.output(new_event_file, events)
     end
 
     def merge_event events, event_added
@@ -77,7 +85,7 @@ module SpanReport::Context
   class IEDurationRule
     def initialize(*args)
       options = args.extract_options!
-      @iename = options[:iename]
+      @iename = options[:iename].to_sym
       @event_name = options[:event_name]
       @duration = options[:duration].to_i
       @threshold = options[:threshold].to_f
@@ -88,13 +96,14 @@ module SpanReport::Context
       @satisfied = false
     end
 
-    # ieinfo is a hash: {ueid: "1", name: "i625", value: "-90", pctime: "1231234"}
+    # ieinfo is a hash: {ueid: "1", i625: -90,  pctime: "1231234"}
     # valid condition is as list:
     # * iename is equal
     # * threshold condition is satisfy
     # * duration time is satisfy
     def valid? ieinfo
-      check = ( ieinfo[:name] == @iename && ieinfo[:value] && value_valid?(ieinfo[:value].to_f) )
+      return false unless ieinfo.has_key? @iename
+      check = ( value_valid?(ieinfo[@iename].to_f) )
       if check && !@satisfied
         @first_satify_time > 0 || @first_satify_time = ieinfo[:pctime].to_i
         return @satisfied = (ieinfo[:pctime].to_i - @first_satify_time > @threshold)
@@ -108,6 +117,7 @@ module SpanReport::Context
 
     def create_event ieinfo
       # ueid,pctime,record_id,ue_version,ue_mode,rec_type from signal
+      event = {}
       event[:ueid] = ieinfo[:ueid]
       event[:pctime] = ieinfo[:pctime]
       event[:record_id] = 999
@@ -116,23 +126,25 @@ module SpanReport::Context
       event[:rec_type] = 1
       event[:event] = @event_name
       event[:extrainfo] = @signal_name
+      event
     end
 
     private
     def value_valid? ievalue
+      result = false
       case @compare_type
       when 0
-        ievalue == @threshold
+        result = ievalue == @threshold
       when 1
-        ievalue < @threshold
+        result = ievalue < @threshold
       when 2
-        ievalue <= @threshold
+        result = ievalue <= @threshold
       when 3
-        ievalue > @threshold
+        result = ievalue > @threshold
       when 4
-        ievalue >= @threshold
+        result = ievalue >= @threshold
       end
     end
-
+    
   end
 end
